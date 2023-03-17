23c4960aa2504c6d3b032e0938fff1b8ddacc9a9
Milo Bonnet <milovtt@gmail.com>
2023-03-17 14:12:08 +0100

    Finish front login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,16 +1,37 @@
-@vite(['resources/scss/login.scss', 'resources/js/app.js'])
-<form method="POST" action="{{ route('login') }}">
-    @csrf
-    <label for="email">Email</label>
-    <input type="email" name="email" id="email" required>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite(['resources/scss/login.scss', 'resources/js/app.js'])
+    <title>Milo Panel | Login</title>
+</head>
+<body>
+    <div class="navbar">
+        <a href="#">Milo'Panel</a>
+        <a href="#" class="settings">Settings</a>
+        <a href="/login" class="connexion">Connexion</a>
+    </div>
+    <div class="container">
+        <div class="top-container">
+            <h2>Connexion au panel</h2>
+            <hr>
+        </div>
+        <form method="POST" action="{{ route('login') }}">
+            @csrf
+            <label for="email">Email:</label>
+            <input type="email" name="email" id="email" placeholder="Votre email"required>
 
-    <label for="password">Mot de passe</label>
-    <input type="password" name="password" id="password" required>
+            <label for="password">Mot de passe:</label>
+            <input type="password" name="password" id="password" placeholder="Mot de passe" required>
 
-    <button type="submit">Se connecter</button>
-</form>
+            <button type="submit">Se connecter</button>
+        </form>
+    </div>
+
 @if ($errors->any())
-    <div>
+    <div class="error">
         <ul>
             @foreach ($errors->all() as $error)
                 <li>{{ $error }}</li>
@@ -18,3 +39,9 @@
         </ul>
     </div>
 @endif
+
+<footer>
+    <img src="/img/Vector.png" alt="" class="vector">
+</footer>
+</body>
+</html>
